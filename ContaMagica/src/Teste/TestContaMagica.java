df502d6319bf3cc92f8335fbc1453e4213fc11dc
00161615 <00161615@nbdti1015944366.lojasrenner.com.br>
2016-09-05 16:22:11 -0300

    Ajuste na classe de tese e no teste de comparaco no metodo deposito

--- ContaMagica/src/Teste/TestContaMagica.java
+++ ContaMagica/src/Teste/TestContaMagica.java
@@ -24,54 +24,63 @@ public class TestContaMagica {
 		conta3 = new ContaMagica("Ivanira");
 		conta4 = new ContaMagica("Edson");
 		conta5 = new ContaMagica("Cristina");
-		this.valor = new BigDecimal(0);
+		this.valor = new BigDecimal(0.0);
 	}
 	
 	@Test
 	public void testeDepositoSilver(){
-		valor.valueOf(0);
+		valor = valor.ZERO;
 		conta1.setSaldo(valor);
-		valor.valueOf(100);
+		valor = valor.valueOf(100.0);
 		conta1.deposito(valor);
 		assertEquals(1,conta1.getStatus());
 	}
 	
 	@Test
 	public void testeDepositoGold(){
-		valor.valueOf(0);
+		valor = valor.ZERO;
 		conta1.setSaldo(valor);
-		valor.valueOf(50000);
+		conta1.setStatus(1);
+		valor = valor.valueOf(60000.0);
 		conta1.deposito(valor);
 		assertEquals(2,conta1.getStatus());
 	}
 	
 	@Test
 	public void testeDepositoPlatinum(){
-		valor.valueOf(0);
+		valor = valor.valueOf(0.0);
 		conta1.setSaldo(valor);
-		valor.valueOf(3000000);
+		conta1.setStatus(1);
+		valor = valor.valueOf(300000.0);
 		conta1.deposito(valor);
 		assertEquals(3,conta1.getStatus());
 	}
 	
 	@Test
 	public void testeSaque(){
-		valor.valueOf(0);
+		valor = valor.ZERO;
 		conta1.setSaldo(valor);
-		valor.valueOf(100000);
+		conta1.setStatus(1);
+		System.out.println("SALDO = " + conta1.getSaldo() + " STATUS = " + conta1.getStatus());
+		valor = valor.valueOf(100000.0);
 		conta1.deposito(valor);
-		valor.valueOf(51000);
+		System.out.println("");
+		System.out.println("SALDO = " + conta1.getSaldo() + " STATUS = " + conta1.getStatus());
+		valor = valor.valueOf(51000.0);
 		conta1.retirada(valor);
+		System.out.println("");
+		System.out.println("SALDO = " + conta1.getSaldo() + " STATUS = " + conta1.getStatus());
 		assertEquals(2,conta1.getStatus());
 	}
 	
 	@Test
 	public void testeSaqueSilver(){
-		valor.valueOf(0);
+		valor = valor.ZERO;
 		conta1.setSaldo(valor);
-		valor.valueOf(2500);
+		conta1.setStatus(1);
+		valor = valor.valueOf(2500.0);
 		conta1.deposito(valor);
-		valor.valueOf(1000);
+		valor = valor.valueOf(1000.0);
 		conta1.retirada(valor);
 		assertEquals(1,conta1.getStatus());
 	}
@@ -79,11 +88,12 @@ public class TestContaMagica {
 	
 	@Test
 	public void testeSaqueGold(){
-		valor.valueOf(0);
+		valor = valor.ZERO;
 		conta1.setSaldo(valor);
-		valor.valueOf(50000);
+		conta1.setStatus(1);
+		valor = valor.valueOf(50000.0);
 		conta1.deposito(valor);
-		valor.valueOf(260000);
+		valor = valor.valueOf(260000.0);
 		conta1.retirada(valor);
 		assertEquals(1,conta1.getStatus());
 	}
@@ -91,13 +101,14 @@ public class TestContaMagica {
 	
 	@Test
 	public void testeSaquePlatinum(){
-		valor.valueOf(0);
+		valor = valor.ZERO;
 		conta1.setSaldo(valor);
-		valor.valueOf(255000);
+		conta1.setStatus(1);
+		valor = valor.valueOf(255000.0);
 		conta1.deposito(valor);
-		valor.valueOf(200000);
+		valor = valor.valueOf(200000.0);
 		conta1.retirada(valor);
-		assertEquals(2,conta1.getStatus());
+		assertEquals(1,conta1.getStatus());
 	}
 	
 
--- ContaMagica/src/app/ContaMagica.java
+++ ContaMagica/src/app/ContaMagica.java
@@ -12,34 +12,39 @@ public class ContaMagica {
 	public ContaMagica(String nome) {
 		this.status = 1;
 		this.nome = nome;
+		saldo = new BigDecimal(0.0);
 	}
 
 	public void deposito(BigDecimal valor) {
 		if (status == 1) {
-			this.saldo.add(valor);
+			saldo = this.saldo.add(valor);
+	
 		} else if (status == 2) {
-			aux = BigDecimal.valueOf(0.01);
-			this.saldo.add(aux.multiply(valor).add(valor));
+			aux = BigDecimal.valueOf(0.0);
+			saldo = this.saldo.add(aux.multiply(valor).add(valor));
 
 		} else if (status == 3) {
 			aux = BigDecimal.valueOf(0.025);
-			this.saldo.add(aux.multiply(valor).add(valor));
+			saldo = this.saldo.add(aux.multiply(valor).add(valor));
 		}
-		if(this.status == 1 && this.saldo.compareTo(new BigDecimal(49999))==1){
-			setStatus(2);
+		
+		if(this.saldo.compareTo(new BigDecimal(49999.0))==1 && this.saldo.compareTo(new BigDecimal(200000.0))==0){
+			this.setStatus(2);
 		}
-		else if(this.status == 2 && this.saldo.compareTo(new BigDecimal(200000))==1){
-			setStatus(3);
+		else if(this.saldo.compareTo(new BigDecimal(200000.0))==1){
+			this.setStatus(3);
 		}
+		
 
 	}
-
+	
 	public void retirada(BigDecimal valor) {
 		this.saldo.subtract(valor);
-		if(this.status == 2 && new BigDecimal(25000).compareTo(saldo)==1){
+		if(new BigDecimal(25000.0).compareTo(this.saldo)==1 && this.saldo.compareTo(new BigDecimal(100000.0))==0){
 			setStatus(1);
 		}
-		else if(this.status == 3 && new BigDecimal(100000).compareTo(saldo)==1){
+		
+		else if(new BigDecimal(100000.0).compareTo(this.saldo)==1){
 			setStatus(2);
 		}
 	}
@@ -59,7 +64,7 @@ public class ContaMagica {
 	 * Status = 2 é Gold 
 	 * Status = 3 é Platinum
 	 */
-	private void setStatus(int num) {
+	public void setStatus(int num) {
 		this.status = num;
 	}
 
